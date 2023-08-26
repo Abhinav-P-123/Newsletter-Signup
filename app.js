9be7c4ced1686d7f384986d2cec41f1af24727f3
Abhinav Palugula <abhinav.palugula@gmail.com>
2023-08-26 11:48:16 +0530

    setup mongoDB

--- app.js
+++ app.js
@@ -1,45 +1,63 @@
 const express = require("express"),
   bodyParser = require("body-parser"),
-  https = require("https"),
+  https = require("https");
+var mongoose = require("mongoose"),
   app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.listen(process.env.PORT || 2000, () => {});
+const port = 2000;
+app.listen(process.env.PORT || port, () => { });
+require("dotenv").config()
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/signup.html");
 });
-app.post("/", function (req, res) {
-  const firstName = req.body.FName,
-    lastName = req.body.LName,
-    email = req.body.email,
-    url = "https://us13.api.mailchimp.com/3.0/lists/b4946f8808";
-  var data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        },
-      },
-    ],
-  };
-  const jsonData = JSON.stringify(data);
-  var options = {
-    method: "POST",
-    auth: "abhi:bee786fe78dc3074dc0b37b46c86d563-us13",
-  };
-  const request = https.request(url, options, function (response) {
-    if (response.statusCode == 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else if (response.statusCode != 200) {
-      res.sendFile(__dirname + "/failure.html");
+
+mongoose.connect(process.env.mongoDBConnectionString)
+
+const emailSchema = new mongoose.Schema({
+  FName: {
+    type: String,
+    required: true,
+  },
+  LName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  }
+});
+app.set("view engine", "ejs")
+const emailDB = mongoose.model("Email", emailSchema);
+
+app.post("/", (req, res) => {
+  const reqData = req.body;
+
+  var emailData = [
+    {
+      FName: reqData.FName,
+      LName: reqData.LName,
+      email: reqData.email,
     }
-  });
-  request.write(jsonData);
-  request.end();
-});
-app.post("/failure", function (req, res) {
-  res.redirect("/");
-});
+  ];
+
+  emailDB.find({ email: reqData.email }).then(data => {
+    if (data.length > 0) {
+      res.sendFile(__dirname + "/failure.html")
+    } else if (data.length == 0) {
+      emailDB.insertMany(emailData).then(data => {
+        if (data.length > 0) {
+          res.sendFile(__dirname + "/success.html")
+        } else {
+          res.sendFile(__dirname + "/failure.html")
+        }
+      })
+    }
+  })
+})
+
+
+app.get("/try", (req, res) => {
+  res.sendFile(__dirname + "/signup.html")
+})
---
+++ node_e99185aa7c83b.tmp.js
@@ -0,0 +1,47 @@
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  https = require("https"),
+  app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+const port = 2000;
+app.listen(process.env.PORT || port, () => {});
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+app.post("/", function (req, res) {
+  const firstName = req.body.FName,
+    lastName = req.body.LName,
+    email = req.body.email,
+    url = "https://us13.api.mailchimp.com/3.0/lists/b4946f8808";
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        },
+      },
+    ],
+  };
+  const jsonData = JSON.stringify(data);
+  var options = {
+    method: "POST",
+    auth: "abhi:bee786fe78dc3074dc0b37b46c86d563-us13",
+  };
+  const request = https.request(url, options, function (response) {
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else if (response.statusCode != 200) {
+      res.sendFile(__dirname + "/failure.html");
+    }
+  });
+  request.write(jsonData);
+  request.end();
+});
+app.post("/failure", function (req, res) {
+  res.redirect("/");
+});
+
