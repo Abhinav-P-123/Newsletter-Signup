a3a89a853add4a4b5be4ac1d2ec3012346d5c2f8
WebDev <99081921+Abhinav-P-123@users.noreply.github.com>
2023-08-26 13:36:16 +0530

    update

--- app.js
+++ app.js
@@ -39,39 +39,6 @@ app.post("/", (req, res) => {
       FName: reqData.FName,
       LName: reqData.LName,
       email: reqData.email,
-=======
-app.listen(process.env.PORT || port, () => {});
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signup.html");
-});
-app.post("/", function (req, res) {
-  const firstName = req.body.FName,
-    lastName = req.body.LName,
-    email = req.body.email,
-    url = "https://us13.api.mailchimp.com/3.0/lists/b4946f8808";
-  var data = {
-    members: [
-      {
-        email_address: email,
-        status: "subscribed",
-        merge_fields: {
-          FNAME: firstName,
-          LNAME: lastName,
-        },
-      },
-    ],
-  };
-  const jsonData = JSON.stringify(data);
-  var options = {
-    method: "POST",
-    auth: "abhi:0cc47a90cbbffd5bbf3812e997f861a1-us13",
-  };
-  const request = https.request(url, options, function (response) {
-    if (response.statusCode == 200) {
-      res.sendFile(__dirname + "/success.html");
-    } else if (response.statusCode != 200) {
-      res.sendFile(__dirname + "/failure.html");
->>>>>>> fbda377af08d5ba98b047f250fe2018cc730790e
     }
   ];
 
